f33302c6b86ff64849e16dd50b6c24349d48f46f
Unknown <perryellis88@gmail.com>
2018-03-22 18:48:53 -0500

    Snake collider working

---
+++ Snakey Snake/Assets/Food.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Developer:  Perry Ellis
+/// Program:    Snakey Snake
+/// Date:       3/17/2018
+/// Purpose:    Be the snake Feed the snake
+/// </summary>
+public class Food : MonoBehaviour {
+
+    public GameObject foodPrefab;
+    public Transform topBorder;
+    public Transform bottemBorder;
+    public Transform leftBorder;
+    public Transform rightBorder;
+
+    // Use this for initialization
+    void Start () {
+        //Will spawn the food
+        InvokeRepeating("FoodSpawn", 3, 4);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //Will spawn pieces of food
+    void FoodSpawn()
+    {
+        //will pistion the food in the borders
+        int x = (int)Random.Range(leftBorder.position.x,rightBorder.position.x);
+        int y = (int)Random.Range(bottemBorder.position.y,topBorder.position.y);
+        //Will instaniate the food
+        Instantiate(foodPrefab,new Vector2(x, y),Quaternion.identity);
+
+    }
+}
---
+++ Snakey Snake/Assets/Snake.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Snake : MonoBehaviour {
+    Vector2 dir = Vector2.right;
+    bool ate = false;
+    List<Transform> tail = new List<Transform>();
+    public GameObject tailPrefab;
+
+    // Use this for initialization
+    void Start () {
+        //Will move the snake
+        InvokeRepeating("Move", 0, 0.5f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // Will let you move with arrow keys
+        if (Input.GetKey(KeyCode.RightArrow))
+            dir = Vector2.right;
+        else if (Input.GetKey(KeyCode.DownArrow))
+            dir = -Vector2.up;   
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            dir = -Vector2.right; 
+        else if (Input.GetKey(KeyCode.UpArrow))
+            dir = Vector2.up;
+
+        //Wil let you move with A,W,S,D
+        if (Input.GetKey(KeyCode.D))
+            dir = Vector2.right;
+        else if (Input.GetKey(KeyCode.S))
+            dir = -Vector2.up;    
+        else if (Input.GetKey(KeyCode.A))
+            dir = -Vector2.right; 
+        else if (Input.GetKey(KeyCode.W))
+            dir = Vector2.up;
+    }
+
+    //Will move the snake into a direction
+    void Move()
+    {
+        Vector2 v = transform.position;
+
+        // Move head into new direction
+        transform.Translate(dir);
+
+        //If snake has eaten insert tail piece
+        if (ate)
+        {
+            //will insert tail piece
+            GameObject i = (GameObject)Instantiate(tailPrefab,v,Quaternion.identity);
+
+            tail.Insert(0, i.transform);
+
+            //will set ate back to false so it wont keep growing
+            ate = false;
+        }
+        //will check if there is a tail
+        else if (tail.Count > 0)
+        {
+            //will have the tail follow the head
+            tail.Last().position = v;
+
+            //will add and remove tail
+            tail.Insert(0, tail.Last());
+            tail.RemoveAt(tail.Count - 1);
+        }
+    }
+
+    //will check and see if the snake has eaten
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        //Will check and see if the snake ate food
+        if (coll.name.StartsWith("food_Prefab"))
+        {
+            //will make the snake longer
+            ate = true;
+
+            //will get rid of the food the snake has eaten
+            Destroy(coll.gameObject);
+        }
+        //will check if snake runs into border or its self
+        else
+        {
+            // ToDo 'You lose' screen
+        }
+    }
+}
